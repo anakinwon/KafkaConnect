cdf64cfe2a531395ad6d4f8b3b6bc24ff635fbed
DESKTOP-0I4G6OG\wyr <anakin.won@gmail.com>
2024-03-17 09:17:54 +0900

    첫번째 커밋

--- scripts/connect_start_log.sh
+++ scripts/connect_start_log.sh
@@ -1,2 +1,2 @@
 log_suffix=`date +"%Y%m%d%H%M%S"`
-$CONFLUENT_HOME/bin/connect-distributed $CONFLUENT_HOME/etc/kafka/connect-distributed.properties 2>&1 | tee -a ~/connect_console_log/connect_console_$log_suffix.log
+/home/anakin/confluent/bin/connect-distributed /home/anakin/confluent/etc/kafka/connect-distributed.properties 2>&1 | tee -a ~/connect_console_log/connect_console_$log_suffix.log
