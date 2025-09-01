deb9ee1b6ddb945d130f2b8b5b015e0fda7e7d92
anakin <anakin.won@gmail.com>
2025-09-01 16:01:49 +0900

    mysql설치

---
+++ cdc_to_jdbc_sample/G_postgres_to_oracle/G10_potoora_cdc_source/RGJS.sh
@@ -0,0 +1,1 @@
+/usr/bin/http POST http://localhost:8083/connectors @/home/anakin/connector_configs/G_postgres_to_oracle/G10_potoora_cdc_source/$1
---
+++ cdc_to_jdbc_sample/G_postgres_to_oracle/G20_potoora_jdbc_sink/RGJS.sh
@@ -0,0 +1,1 @@
+/usr/bin/http POST http://localhost:8083/connectors @/home/anakin/connector_configs/G_postgres_to_oracle/G20_potoora_jdbc_sink/$1
